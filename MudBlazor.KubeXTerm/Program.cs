a8b011af3d4bcdcdb5c4a46fddc288b651204863
David <freeman412@gmail.com>
2025-01-08 18:08:41 -0500

    Add project files.

---
+++ MudBlazor.KubeXTerm/Program.cs
@@ -0,0 +1,32 @@
+using MudBlazor.KubeXTerm.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<HttpClient>(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5064") // Set to your API's base URL
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
