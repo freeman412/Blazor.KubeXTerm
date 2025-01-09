8507f6a2dce4fefd1d8eaf662defb51a24d253bc
David <freeman412@gmail.com>
2025-01-08 23:02:37 -0500

    refactoring and initial tabbed version working

---
+++ MudBlazor.KubeXTerm/wwwroot/js/terminalResize.js
@@ -0,0 +1,23 @@
+﻿let resizeCallback;
+
+window.registerViewportChangeCallback = (dotnetHelper) => {
+    // Save the callback to remove it later
+    resizeCallback = () => {
+        dotnetHelper.invokeMethodAsync('OnResize', window.innerWidth, window.innerHeight);
+    };
+
+    // Attach event listeners
+    window.addEventListener('load', resizeCallback);
+    window.addEventListener('resize', resizeCallback);
+};
+
+window.unregisterViewportChangeCallback = () => {
+    if (resizeCallback) {
+        // Remove event listeners
+        window.removeEventListener('load', resizeCallback);
+        window.removeEventListener('resize', resizeCallback);
+
+        // Clear the saved reference
+        resizeCallback = null;
+    }
+};
