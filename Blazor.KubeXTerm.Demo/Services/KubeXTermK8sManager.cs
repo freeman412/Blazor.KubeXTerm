e7c9296966c2855980e6de49c9f46f46ac8d3e43
David <freeman412@gmail.com>
2025-02-17 09:38:01 -0500

    refactor started

---
+++ Blazor.KubeXTerm.Demo/Services/KubeXTermK8sManager.cs
@@ -0,0 +1,9 @@
+﻿using k8s;
+
+namespace Blazor.KubeXTerm.Demo.Services;
+
+public class KubeXTermK8SManager
+{
+    public KubernetesClientConfiguration K8SConfig;
+    public IKubernetes K8SClient;
+}
